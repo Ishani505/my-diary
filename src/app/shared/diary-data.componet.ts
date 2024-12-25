63480c47d5c3447b42293aa902914ff3142d2ab7
Ishani505 <igunarathne7@gmail.com>
2024-12-25 12:49:32 +0530

    Subjects for delete button

--- src/app/shared/diary-data.componet.ts
+++ src/app/shared/diary-data.componet.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@angular/core";
+import { map, Subject } from "rxjs";
 import { DiaryEntry } from "./diary-entry.model";
 
 @Injectable({providedIn:"root"})
 export class DiaryDataService{
+
+    public diarySubject = new Subject<DiaryEntry[]>();
 
     diaryEntries: DiaryEntry[] = [
         new DiaryEntry("Dec 25th", "Entry 1"),
@@ -12,5 +15,6 @@ export class DiaryDataService{
 
     onDelete(index: Number){
         this.diaryEntries.splice(+index, 1);
+        this.diarySubject.next(this.diaryEntries);
     }
 }
