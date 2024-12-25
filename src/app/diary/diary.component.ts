8daa6eaf7b42c4b4f975d95baef79aea25892f1d
Ishani505 <igunarathne7@gmail.com>
2024-12-25 11:35:27 +0530

    Shared Data Service Created

--- src/app/diary/diary.component.ts
+++ src/app/diary/diary.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DiaryDataService } from '../shared/diary-data.componet';
+import { DiaryEntry } from '../shared/diary-entry.model';
 
 @Component({
   selector: 'app-diary',
   templateUrl: './diary.component.html',
   styleUrls: ['./diary.component.css']
 })
-export class DiaryComponent {
+export class DiaryComponent implements OnInit {
+
+  diaryEntries: DiaryEntry[];
+
+  constructor(private diaryDataService: DiaryDataService) {}
+
+  ngOnInit(): void {
+    this.diaryEntries = this.diaryDataService.diaryEntries;
+  }
 
 }
---
+++ src/app/shared/diary-data.componet.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@angular/core";
+import { DiaryEntry } from "./diary-entry.model";
+
+@Injectable({providedIn:"root"})
+export class DiaryDataService{
+
+    diaryEntries: DiaryEntry[] = [
+        new DiaryEntry("Dec 25th", "Entry 1"),
+        new DiaryEntry("Dec 26th", "Entry 2"),
+        new DiaryEntry("Dec 23rd", "Shared data service entry")
+    ]
+}
