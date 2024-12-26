603608279f13515406559a09f1c12cab8f62050d
Ishani505 <igunarathne7@gmail.com>
2024-12-26 17:52:20 +0530

    registered URIs, and fixed the error with ugly navbar

--- backend/rest.js
+++ backend/rest.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.send('hello from express');
+diaryEntries = [
+    { id: 1, date: "March 1st", entry: "Entry1" },
+    { id: 2, date: "March 2nd", entry: "Entry2" },
+    { id: 3, date: "March 3rd", entry: "Entry3" },
+];
+
+app.use('/diary-entries',(req, res, next) => {
+    res.json({'diaryEntries': diaryEntries});
+
 })
 
 module.exports = app;
--- src/app/shared/diary-data.componet.ts
+++ src/app/shared/diary-data.componet.ts
@@ -4,18 +4,29 @@ import { DiaryEntry } from "./diary-entry.model";
 
 @Injectable({providedIn:"root"})
 export class DiaryDataService{
+
+
+    updateEntry(index: number, entry: DiaryEntry) {
+            this.diaryEntries[index] = entry;
+            this.diarySubject.next(this.diaryEntries);
+        }
     
 
     public diarySubject = new Subject<DiaryEntry[]>();
 
     diaryEntries: DiaryEntry[] = [
-        new DiaryEntry("Dec 25th", "Entry 1"),
-        new DiaryEntry("Dec 26th", "Entry 2"),
-        new DiaryEntry("Dec 23rd", "Shared data service entry")
+        new DiaryEntry(1,"Dec 25th", "Entry 1"),
+        new DiaryEntry(2,"Dec 26th", "Entry 2"),
+        new DiaryEntry(3,"Dec 23rd", "Shared data service entry")
     ]
 
     onDelete(index: Number){
         this.diaryEntries.splice(+index, 1);
+        this.diarySubject.next(this.diaryEntries);
+    }
+
+    onDeleteEntry(id: Number){
+        this.diaryEntries.splice(+id, 1);
         this.diarySubject.next(this.diaryEntries);
     }
 
@@ -28,6 +39,13 @@ export class DiaryDataService{
     getDiaryEntry(index: number){
       
         return{...this.diaryEntries[index]};
+        //return this.diaryEntries;
+    }
+
+    getDiaryEntries(){
+      
+        //return{...this.diaryEntries[index]};
+        return this.diaryEntries;
     }
 
     onUpdateEntry(paramId: number, newEntry: DiaryEntry) {
